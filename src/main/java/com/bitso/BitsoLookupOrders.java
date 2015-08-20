764e7969490bf5cb7c45dfdd124fe9e7ce9b673f
Daniel Vogel <dvogel@cs.stanford.edu>
2015-08-19 21:30:39 -0500

    add a BitsoLookupOrders class (maybe to be deleted afterwards)

---
+++ src/main/java/com/bitso/BitsoLookupOrders.java
@@ -0,0 +1,37 @@
+package com.bitso;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.bitso.exchange.BookOrder;
+
+public class BitsoLookupOrders {
+
+    public ArrayList<BookOrder> list;
+
+    public BitsoLookupOrders(String lookupOrderJSON) {
+        JSONArray obj = new JSONArray(lookupOrderJSON);
+        list = new ArrayList<BookOrder>(obj.length());
+
+        for (int i = 0; i < obj.length(); i++) {
+            JSONObject o = obj.getJSONObject(i);
+            BookOrder order = Bitso.processBookOrderJSON(o.toString());
+            if (order != null) {
+                list.add(order);
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("====LOOKUP_ORDERS====");
+        if (list != null) {
+            for (BookOrder o : list) {
+                sb.append(o.toString());
+            }
+        }
+        return sb.toString();
+    }
+}
