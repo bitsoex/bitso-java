e339bf4524ee62ecb3141d35482a5af72620c560
Nelson Victor Cruz Hdez <victor.cruz.isc@gmail.com>
2017-01-23 19:20:28 -0600

    Add support for mocks
    Signed-off-by: Nelson Victor Cruz Hdez <victor.cruz.isc@gmail.com>

---
+++ src/test/java/com/bitso/BitsoMockJSON.java
@@ -0,0 +1,47 @@
+package com.bitso;
+
+import org.json.JSONObject;
+
+import com.bitso.helpers.Helpers;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BitsoMockJSON {
+    private static final String PATH = "src/test/java/JSONFiles/";
+    
+    public static JSONObject getJSONFromFile(String fileName) {
+        String jsonString = getJSONString(fileName);
+        if(jsonString == null){
+            return null;
+        }
+        return Helpers.parseJson(jsonString);
+    }
+
+    private static String getJSONString(String fileName) {
+        BufferedReader br = null;
+        String line = "";
+        StringBuffer sb = new StringBuffer();
+        try {
+            FileReader fr = new FileReader(PATH + fileName);
+            br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            line = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            line = null;
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return line;
+    }
+}
---
+++ src/test/java/com/bitso/BitsoMockTest.java
@@ -0,0 +1,81 @@
+package com.bitso;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import com.bitso.exchange.BookInfo;
+import com.bitso.exchange.Ticker;
+
+public class BitsoMockTest {
+    private Bitso bitso;
+    private ArrayList<BookInfo> mockAvailableBooks;
+    private BitsoTicker mockTicker;
+    
+    @Before
+    public void setUp() throws Exception{
+        bitso = Mockito.mock(Bitso.class);
+        setUpTestMocks();
+        Mockito.when(bitso.availableBooks()).thenReturn(mockAvailableBooks);
+        Mockito.when(bitso.getTicker(BitsoBook.BTC_MXN)).thenReturn(mockTicker);
+        //Mockito.when(bitso.).thenReturn();
+    }
+    
+    @Test
+    public void testAvailableBooks() {
+        ArrayList<BookInfo> books = bitso.availableBooks();
+        assertEquals(true, (books != null));
+        for (BookInfo bi : books) {
+            assertEquals(nullCheck(bi, BookInfo.class), true);
+        }
+        assertEquals(books.size() > 0, true);
+    }
+
+    @Test
+    public void testTicker() {
+        BitsoTicker bb = bitso.getTicker(BitsoBook.BTC_MXN);
+        assertEquals(nullCheck(bb, Ticker.class), true);
+        assertEquals(nullCheck(bb, BitsoTicker.class), true);
+    }
+    
+    private void setUpTestMocks(){
+        setUpAvailableBooks(BitsoMockJSON.getJSONFromFile("availableBooks.json"));
+        setUpTicker(BitsoMockJSON.getJSONFromFile("ticker.json"));
+    }
+    
+    private void setUpAvailableBooks(JSONObject o){
+        mockAvailableBooks = new ArrayList<BookInfo>();
+        JSONArray arr = o.getJSONArray("payload");
+        for (int i = 0; i < arr.length(); i++) {
+            mockAvailableBooks.add(new BookInfo(arr.getJSONObject(i)));
+        }
+    }
+    
+    private void setUpTicker(JSONObject o){
+        mockTicker = new BitsoTicker(o.getJSONObject("payload"));
+    }
+    
+    private boolean nullCheck(Object obj, Class<?> genericType) {
+        Field[] fields = genericType.getDeclaredFields();
+        for (Field f : fields) {
+            try {
+                if (f.get(obj) == null){
+                    System.out.println(f.getName() + " attribute is null");
+                    return false;
+                }
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+
+    }
+}
