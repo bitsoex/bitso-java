c2cbbd149e400ae63924c66344de669ede9b8045
Nelson Victor Cruz Hdez <victor.cruz.isc@gmail.com>
2017-01-30 17:24:37 -0600

    Verify if server test will be performed
    Add junit.Assume to check if test that require server requests
    will be performed, based on the existance of environment variables.
    
    Signed-off-by: Nelson Victor Cruz Hdez <victor.cruz.isc@gmail.com>

--- src/test/java/com/bitso/BitsoServerTest.java
+++ src/test/java/com/bitso/BitsoServerTest.java
@@ -2,6 +2,8 @@ package com.bitso;
 
 import static org.junit.Assert.assertEquals;
 import java.util.Map;
+
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,6 +12,12 @@ public class BitsoServerTest extends BitsoTest {
     public void setUp() throws Exception {
         String secret = System.getenv("BITSO_DEV_PRIVATE");
         String key = System.getenv("BITSO_DEV_PUBLIC_KEY");
+
+        // If BITSO_DEV_PRIVATE and BITSO_DEV_PUBLIC_KEY
+        // environment variables are set, tests will be executed
+        // normally, otherwise, they will be ignored.
+        Assume.assumeTrue((secret != null && key != null));
+
         mBitso = new Bitso(key, secret, 0, true, false);
     }
 
