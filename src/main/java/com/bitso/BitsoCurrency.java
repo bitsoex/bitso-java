82db094d37c1f0d8c858df600bbc9e73c8f5b5cc
Daniel Vogel <dvogel@cs.stanford.edu>
2017-08-05 18:11:18 -0500

    adding support for BCH in apiv2

--- src/main/java/com/bitso/BitsoCurrency.java
+++ src/main/java/com/bitso/BitsoCurrency.java
@@ -2,7 +2,7 @@ package com.bitso;
 
 public enum BitsoCurrency {
 
-    BTC, MXN, ETH, XRP;
+    BCH, BTC, ETH, MXN, XRP;
 
     public String toString() {
         return this.name().toLowerCase();
