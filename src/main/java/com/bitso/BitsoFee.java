d6a04d0edeaa918dbb9ce736fdc625e383adae4c
Carlos Omana <araogaraog@gmail.com>
2018-06-08 15:27:06 -0500

    Add maker and taker to fee model
    Signed-off-by: Carlos Omana <araogaraog@gmail.com>

--- src/main/java/com/bitso/BitsoFee.java
+++ src/main/java/com/bitso/BitsoFee.java
@@ -1,14 +1,13 @@
 package com.bitso;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
+import com.bitso.helpers.Helpers;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import com.bitso.helpers.Helpers;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class BitsoFee {
     private HashMap<String, Fee> mTradeFees;
@@ -27,7 +26,12 @@ public class BitsoFee {
             JSONObject fee = jsonFees.getJSONObject(i);
             String book = Helpers.getString(fee, "book");
             Fee currentFee = new Fee(book, Helpers.getBD(fee, "fee_decimal"),
-                    Helpers.getBD(fee, "fee_percent"));
+                    Helpers.getBD(fee, "fee_percent"),
+                    Helpers.getBD(fee, "taker_fee_decimal"),
+                    Helpers.getBD(fee, "taker_fee_percent"),
+                    Helpers.getBD(fee, "maker_fee_decimal"),
+                    Helpers.getBD(fee, "maker_fee_percent")
+            );
             mTradeFees.put(book, currentFee);
         }
     }
@@ -66,12 +70,21 @@ public class BitsoFee {
         private String mBook;
         private BigDecimal mFeeDecimal;
         private BigDecimal mFeePercent;
+        private BigDecimal mTakerFeeDecimal;
+        private BigDecimal mTakerFeePercent;
+        private BigDecimal mMakerFeeDecimal;
+        private BigDecimal mMakerFeePercent;
 
-        public Fee(String mBook, BigDecimal mFeeDecimal, BigDecimal mFeePercent) {
+        public Fee(String mBook, BigDecimal mFeeDecimal, BigDecimal mFeePercent, BigDecimal mTakerFeeDecimal,
+                   BigDecimal mTakerFeePercent, BigDecimal mMakerFeeDecimal, BigDecimal mMakerFeePercent) {
             super();
             this.mBook = mBook;
             this.mFeeDecimal = mFeeDecimal;
             this.mFeePercent = mFeePercent;
+            this.mTakerFeeDecimal = mTakerFeeDecimal;
+            this.mTakerFeePercent = mTakerFeePercent;
+            this.mMakerFeeDecimal = mMakerFeeDecimal;
+            this.mMakerFeePercent = mMakerFeePercent;
         }
 
         public String getBook() {
@@ -101,5 +114,22 @@ public class BitsoFee {
         public String toString() {
             return Helpers.fieldPrinter(this, BitsoFee.Fee.class);
         }
+
+        public BigDecimal getTakerFeePercent() {
+            return mTakerFeePercent;
+        }
+
+        public BigDecimal getTakerFeeDecimal() {
+            return mTakerFeeDecimal;
+        }
+
+        public BigDecimal getMakerFeePercent() {
+            return mMakerFeePercent;
+        }
+
+        public BigDecimal getMakerFeeDecimal() {
+            return mMakerFeeDecimal;
+        }
+
     }
 }
