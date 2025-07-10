a99f054feb9afb93472c34b348d62401036d4d3b
Enrique Zamudio <alterbass@gmail.com>
2025-07-10 17:01:00 -0600

    add processed-orders channel

--- src/main/java/com/bitso/websockets/BitsoChannels.java
+++ src/main/java/com/bitso/websockets/BitsoChannels.java
@@ -1,11 +1,17 @@
 package com.bitso.websockets;
 
+/** Defines the channels that are available in the public websockets.
+ */
 public enum BitsoChannels {
-    TRADES("trades"), DIFF_ORDERS("diff-orders"), ORDERS("orders"), KA("ka");
+    TRADES("trades"),
+    DIFF_ORDERS("diff-orders"),
+    ORDERS("orders"),
+    PROCESSED_ORDERS("processed-orders"),
+    KA("ka");
 
     private final String id;
 
-    private BitsoChannels(String id) {
+    BitsoChannels(String id) {
         this.id = id;
     }
 
@@ -14,12 +20,19 @@ public enum BitsoChannels {
     }
 
     public static BitsoChannels getBitsoChannel(String data) {
-        BitsoChannels channel = null;
-        if (data.equals("trades")) channel = BitsoChannels.TRADES;
-        if (data.equals("diff-orders")) channel = DIFF_ORDERS;
-        if (data.equals("orders")) channel = ORDERS;
-        if (data.equals("ka")) channel = KA;
-        return channel;
+        switch (data) {
+            case "trades":
+               return BitsoChannels.TRADES;
+            case "diff-orders":
+                return DIFF_ORDERS;
+            case "orders":
+                return ORDERS;
+            case "ka":
+                return KA;
+            case "processed-orders":
+                return PROCESSED_ORDERS;
+        }
+        return null;
     }
 
 }
